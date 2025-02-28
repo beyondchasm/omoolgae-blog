f59d8d59bcfda7867e9fcfa632c052962c133778
createogu <createogu@gmail.com>
2025-02-28 22:32:59 +0900

    애드센스 도전

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -204,6 +204,8 @@ export default (() => {
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
           .map((res) => JSResourceToScriptElement(res, true))}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2668656366673635"
+          crossorigin="anonymous"></script>
       </head>
     )
   }
